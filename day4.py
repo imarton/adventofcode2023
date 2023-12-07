dae57e68d287eb43b5cef774530938c484d35007
imarton <imarton@users.noreply.github.com>
2023-12-07 11:15:55 +0100

    day4 part2 is done

--- day4.py
+++ day4.py
@@ -31,5 +31,62 @@ def getDeckScore(fileName):
     return result
 
 
+def processline(line):
+    name, values = line.replace('\n', '').split(': ')
+    id = int(list(filter(None, name.split(' ')))[1])
+
+    winner, yours = values.split(' | ')
+    winner = list(filter(None, winner.split(' ')))
+    yours = list(filter(None, yours.split(' ')))
+    # print('w:', winner)
+    # print('y:', yours)
+    cnt = 0
+    for y in yours:
+        if y in winner:
+            cnt += 1
+
+    return id, cnt
+
+
+def getDeckSize(fileName):
+    cardTypes = {} # azonosító : nyertes számok mennyisége
+    cardQueue = {} # feldolgozásra váró kártyák (id : darabszam)
+    result = 0
+    f = open(fileName, "r")
+
+    # kezdőpakli felolvasása, queue-ba helyezése
+    while True:
+        line = f.readline()
+        if len(line) == 0:
+            break
+        cid, cnum = processline(line)
+
+        if cid not in cardTypes:
+            cardTypes[cid] = cnum
+
+        if cid in cardQueue:
+            cardQueue[cid] += 1
+        else:
+            cardQueue[cid] = 1
+
+    f.close()
+    # print('types:', cardTypes, ' queue:', cardQueue)
+
+    # queue feldolgozása
+    i = 1
+    while i <= len(cardTypes):
+        if cardQueue[i] <= 0:
+            i +=1
+        else:
+            result += 1
+            cardQueue[i] -= 1
+            cnt = cardTypes[i]
+            for j in range(cnt):
+                cardQueue[i+j+1] +=1
+
+    return result
+
+
 if __name__ == '__main__':
     print('Part1:', getDeckScore("day4_input.txt"))
+    print('Part2:', getDeckSize("day4_input.txt"))
--- test_day4.py
+++ test_day4.py
@@ -8,7 +8,14 @@ class Test(TestCase):
         sample = 'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
         self.assertEqual(day4.getCardScore(sample), 8)
 
-
     def test_get_deck_score(self):
-
         self.assertEqual(day4.getDeckScore("day4_sample.txt"), 13)
+
+    def test_get_deck_size(self):
+        self.assertEqual(30, day4.getDeckSize("day4_sample.txt"))
+
+    def test_processline(self):
+        self.assertEqual((1, 4), day4.processline('Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'))
+        self.assertEqual((2, 2), day4.processline('Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19'))
+        self.assertEqual((6, 0), day4.processline('Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11'))
+
