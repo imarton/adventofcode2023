157497fec847df97e04f7dc69f4e97267dac6a4d
Márton István <martoni779@gmail.com>
2023-12-09 18:15:33 +0100

    day6 part1 done

--- day6.py
+++ day6.py
@@ -1,8 +1,40 @@
+
 def loadData(filename):
-    pass
+    races = []
+    with open(filename) as f:
+        d = f.readlines()
+        print(d)
+        row1 = list(filter(None, d[0].split(' ')))
+        row2 = list(filter(None, d[1].split(' ')))
+        for i in range(len(row1)):
+            if i == 0:
+                continue
+            races.append((int(row1[i]), int(row2[i])))
+
+    return races
+
+
+def numOfWin(race):
+    res = 0
+    time = race[0]
+    lastRecord = race[1]
+    for t in range(time+1):
+        d = (time - t) * t
+        if lastRecord < d:
+            res += 1
+
+    return res
+
+
+def getNumberOfBeating(races):
+    result = 1
+    for r in races:
+        result *= numOfWin(r)
+
+    return result
 
 
 if __name__ == '__main__':
-    loadData("day5_sample.txt")
-    print('Part1:')
+    races = loadData("day6_input.txt")
+    print('Part1:', getNumberOfBeating(races))
     # print('Part2:')
--- test_day6.py
+++ test_day6.py
@@ -1,6 +1,16 @@
 from unittest import TestCase
 
+import day6
+
 
 class Test(TestCase):
     def test_load_data(self):
-        self.fail()
+        self.assertEqual([(7, 9), (15, 40), (30, 200)], day6.loadData("day6_sample.txt"))
+
+    def test_get_number_of_beating(self):
+        r = day6.loadData("day6_sample.txt")
+        self.assertEqual(288, day6.getNumberOfBeating(r))
+
+    def test_num_of_win(self):
+        inp = (7, 9)
+        self.assertEqual(4, day6.numOfWin(inp))
