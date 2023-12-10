0899fc03ad547c10103f1834f9ced03ef47146c3
Márton István <martoni779@gmail.com>
2023-12-10 21:53:07 +0100

    day7 part1 done

--- day7.py
+++ day7.py
@@ -1,9 +1,104 @@
 # Camel Cards
 
+# Hand list
+game = []
+handtypes = ['Five of a kind', 'Four of a kind', 'Full house', 'Three of a kind', 'Two pair', 'One pair', 'High card']
+cardvalues = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+
+
+class Hand:
+    def __init__(self, cards, bid, type=-1):
+        self.card = cards
+        self.bid = bid
+        self.type = type
+
+    def __str__(self):
+        return f"Hand(card={self.card}, bid={self.bid})"
+
+    __repr__ = __str__
+
+    def __eq__(self, other):
+        return self.__str__() == other.__str__()
+
+    def __hash__(self):
+        return self.card, self.bid
+
+    def __lt__(self, other):
+        if self.type < other.type or self.__str__() == other.__str__():
+            return False
+
+        if self.type > other.type:
+            return True
+
+        # self.type == other.type
+        for i in range(len(self.card)):
+            if self.card[i] == other.card[i]:
+                continue
+            if cardvalues[self.card[i]] < cardvalues[other.card[i]]:
+                return True
+            else:
+                return False
+
+
+
+def getType(cards):
+    charCount = [cards.count(c) for c in set(cards)]
+
+    if len(charCount) == 1:
+        return 0
+    elif len(charCount) == 2:
+
+        if charCount[0] == 4 or charCount[0] == 1:
+            return 1
+        else:
+            return 2
+
+    elif len(charCount) == 3:
+        if charCount[0] == 3 or charCount[1] == 3 or charCount[2] == 3:
+            return 3
+        else:
+            return 4
+
+    elif len(charCount) == 4:
+        return 5
+    elif len(charCount) == 5:
+        return 6
+
 
 def getTotalPrice():
-    pass
+    tmp = []
+    for h in game:
+        if len(tmp) == 0:
+            tmp.append(h)
+            continue
+
+        inserted = False
+        for i in range(len(tmp)):
+            if h < tmp[i]:
+                tmp.insert(i, h)
+                inserted = True
+                break
+
+        if not inserted:
+            tmp.append(h)
+
+    sum = 0
+    for i, h in enumerate(tmp):
+        sum += (i+1)*h.bid
+
+    return sum
+
+
+
+
+
+def loadDatas(filename):
+    with open(filename, 'r') as f:
+        for row in f:
+            cards, bid = row.split(' ')
+            game.append(Hand(cards, int(bid), getType(cards)))
 
 
 if __name__ == '__main__':
+    loadDatas('day7_input.txt')
     print('part1: ', getTotalPrice())
--- test_day7.py
+++ test_day7.py
@@ -1,6 +1,38 @@
 from unittest import TestCase
 
+import day7
+
 
 class Test(TestCase):
+
     def test_get_total_price(self):
-        self.fail()
+        day7.loadDatas('day7_sample.txt')
+        self.assertEqual(6440, day7.getTotalPrice())
+
+    def test_get_type(self):
+        self.assertEqual('Five of a kind', day7.handtypes[day7.getType('AAAAA')])
+        self.assertEqual('Four of a kind', day7.handtypes[day7.getType('AA8AA')])
+        self.assertEqual('Full house', day7.handtypes[day7.getType('23332')])
+        self.assertEqual('Three of a kind', day7.handtypes[day7.getType('TTT98')])
+        self.assertEqual('Two pair', day7.handtypes[day7.getType('23432')])
+        self.assertEqual('One pair', day7.handtypes[day7.getType('A23A4')])
+        self.assertEqual('High card', day7.handtypes[day7.getType('23456')])
+
+    def test_load_datas(self):
+        day7.loadDatas('day7_sample.txt')
+        h = day7.Hand('32T3K', 765)
+        self.assertIn(h, day7.game)
+        h = day7.Hand('T55J5', 684)
+        self.assertIn(h, day7.game)
+        h = day7.Hand('KK677', 28)
+        self.assertIn(h, day7.game)
+        h = day7.Hand('KTJJT', 220)
+        self.assertIn(h, day7.game)
+        h = day7.Hand('QQQJA', 483)
+        self.assertIn(h, day7.game)
+
+    def test_compare_to(self):
+        h1 = day7.Hand('AAAAQ', 765, day7.getType('AAAAQ'))
+        h2 = day7.Hand('AAAAK', 765, day7.getType('AAAAT'))
+        self.assertLess(h1, h2)
+
