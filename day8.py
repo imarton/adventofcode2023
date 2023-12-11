be4e3a743000455a4899d7908274ac45e183246c
Márton István <martoni779@gmail.com>
2023-12-11 10:06:59 +0100

    day8 part1 done

--- day8.py
+++ day8.py
@@ -1,6 +1,43 @@
+direction = ''
+map1 = {}
+
+
 def steps():
-    pass
+    st = 0
+    loc = 'AAA'
+    i = 0
+    while 'ZZZ' != loc:
+        loc = map1[loc][direction[i]]
+        st += 1
+        i += 1
+        if i == len(direction):
+            i = 0
+
+    return st
+
+
+def loadData(filename):
+    global direction
+    global map1
+
+    direction = None
+    map1 = {}
+    with open(filename, 'r') as f:
+        firts = True
+        for row in f:
+            if firts:
+                direction = row.replace('\n', '')
+                firts = False
+                continue
+
+            if row.strip() == '':
+                continue
+
+            key, v = row.replace('\n', '').split(' = ')
+            vl, vr = v[1:-1].split(', ')
+            map1[key] = {'L': vl, 'R': vr}
 
 
 if __name__ == "__main__":
-    print('Part1:')
+    loadData('day8_input.txt')
+    print('Part1:', steps())
--- test_day8.py
+++ test_day8.py
@@ -1,6 +1,17 @@
 from unittest import TestCase
 
+import day8
+
 
 class Test(TestCase):
     def test_steps(self):
-        self.fail()
+        day8.loadData('day8_sample.txt')
+        self.assertEqual(2, day8.steps())
+        day8.loadData('day8_sample2.txt')
+        self.assertEqual(6, day8.steps())
+
+    def test_load_data(self):
+        day8.loadData('day8_sample.txt')
+        self.assertEqual('RL', day8.direction)
+
+        self.assertEqual({'L': 'ZZZ', 'R': 'GGG'}, day8.map1['CCC'])
