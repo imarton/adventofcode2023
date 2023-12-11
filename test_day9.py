5ea1ee71b20f344a47dd8d5e310aec0d59b2d9bc
Márton István <martoni779@gmail.com>
2023-12-11 20:56:04 +0100

    day9 part1 done

--- test_day9.py
+++ test_day9.py
@@ -1,6 +1,14 @@
 from unittest import TestCase
 
+import day9
+
 
 class Test(TestCase):
     def test_prediction(self):
-        self.fail()
+        self.assertEqual(114, day9.prediction("day9_sample.txt"))
+
+    def test_interpolate(self):
+        self.assertEqual(18, day9.nextElem([0, 3, 6, 9, 12, 15]))
+        self.assertEqual(28, day9.nextElem([1, 3, 6, 10, 15, 21]))
+        self.assertEqual(68, day9.nextElem([10, 13, 16, 21, 30, 45]))
+
