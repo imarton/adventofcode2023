8b064e4aa3847cc623a4e0dbd0e23f936e02b5c4
imarton <imarton@users.noreply.github.com>
2023-12-12 15:58:30 +0100

    day10 part1 done

--- day10.py
+++ day10.py
@@ -1,6 +1,94 @@
-def farthest(filename):
-    pass
+import math
+
+pipemap = []
+startPoint = None
+# Remark: Coordinate system starts at the top-left corner (0,0)
+# first elem postition: (1,1)
+
+# pipe type + shifting koords
+pipes = {'|': [(0, 1), (0, -1)],
+         'L': [(0, -1), (1, 0)],
+         '-': [(1, 0), (-1, 0)],
+         'J': [(-1, 0), (0, -1)],
+         'F': [(1, 0), (0, 1)],
+         '7': [(-1, 0), (0, 1)]}
+
+
+class Node:
+    def __init__(self, point, type):
+        self.point = point  # point (x, y)
+        self.type = type  # type of pipe: |, L, F, J, 7, -
+
+    def __str__(self):
+        return f"{self.type} {self.point}"
+
+    __repr__ = __str__
+
+    def __eq__(self, other):
+        return self.point == other.point and self.type == other.type
+
+    def __hash__(self):
+        return hash((self.point, self.type))
+
+
+def firstStep(startnode):
+    """
+    search the proper direction out of 'S'
+    :param startnode:
+    :return: nextnode
+    """
+    if startnode.type == 'S':
+        xs = startnode.point[0]
+        ys = startnode.point[1]
+        if pipemap[ys][xs + 1] in '-J7':
+            return Node((xs + 1, ys), pipemap[ys][xs + 1])
+        elif pipemap[ys][xs - 1] in '-FL':
+            return Node((xs - 1, ys), pipemap[ys][xs - 1])
+        if pipemap[ys + 1][xs] in '|LJ':
+            return Node((xs, ys + 1), pipemap[ys + 1][xs])
+        elif pipemap[ys - 1][xs] in '|F7':
+            return Node((xs, ys - 1), pipemap[ys - 1][xs])
+
+
+def step(node, prevnode):
+    for shift in pipes[node.type]:
+        tmp = (node.point[0] + shift[0], node.point[1] + shift[1])
+        if prevnode is None or tmp != prevnode.point:
+            return Node(tmp, pipemap[tmp[1]][tmp[0]])
+
+
+def farthest(startp):
+    startnode = Node(startp, 'S')
+    path = []
+
+    node = firstStep(startnode)
+    prevnode = startnode
+    while node != startnode:
+        if path:  # list is not empty
+            prevnode = path[-1]
+        path.append(node)
+        node = step(node, prevnode)
+
+    # len(path) = 20 --> 10
+    # len(path) = 21 --> 11
+    return math.ceil(len(path) / 2)
+
+
+def loaddata(filename):
+    with open(filename, 'r') as f:
+        y = 1
+        for row in f:
+            tmp = '.' + row.replace('\n', '') + '.'
+            pipemap.append(tmp)
+            x = tmp.find('S')
+            if x != -1:
+                startpoint = (x, y)
+            y += 1
+        pipemap.append('.' * len(pipemap[0]))
+        pipemap.insert(0, '.' * len(pipemap[0]))
+    return startpoint
 
 
 if __name__ == "__main__":
-    print("Part1:", farthest("day10_input.txt"))
+    startPoint = loaddata("day10_input.txt")
+    print("Part1:", farthest(startPoint))
--- test_day10.py
+++ test_day10.py
@@ -1,6 +1,26 @@
 from unittest import TestCase
 
+import day10
+
 
 class Test(TestCase):
     def test_farthest(self):
-        self.fail()
+        start = day10.loaddata("day10_sample.txt")
+        self.assertEqual(8, day10.farthest(start))
+
+    def test_loaddata(self):
+        start = day10.loaddata("day10_sample.txt")
+        # print('Start at:', s)
+        # for row in day10.pipemap:
+        #     print(row)
+        self.assertEqual((1, 3), start)
+
+    def test_step(self):
+        start = day10.loaddata("day10_sample.txt")
+        node = day10.step(day10.Node((1, 4), '|'), day10.Node(start, 'S'))
+        self.assertEqual('L', node.type)
+
+    def test_first_step(self):
+        start = day10.loaddata("day10_sample.txt")
+        node = day10.firstStep(day10.Node(start, 'S'))
+        self.assertIn(node.type, '|J')
