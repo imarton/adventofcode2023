ddf01cc5cbf269ba013339fb84479689f22a7951
Márton István <martoni779@gmail.com>
2023-12-14 20:53:21 +0100

    day11 part1 and part2 done

--- day11.py
+++ day11.py
@@ -1,12 +1,62 @@
+# coords of galaxies [(4, 1), (8, 2), ... (x, y)]   1 <= x,y <= len
+galaxies = []
+
+spacex = []
+spacey = []
+
 def loaddata(filename):
     with open(filename, 'r') as f:
+        tmp = set()
+        max = 0
         y = 1
         for row in f:
-            for x in range(len(row)):
-                pass
+            row = row.replace('\n', '')
+            if max == 0:
+                max = len(row)
+
+            if '#' not in row:
+                spacey.append(y)
+            else:
+                for x in range(len(row)):
+                    if row[x] == '#':
+                        galaxies.append([x + 1, y])
+                        tmp.add(x + 1)
+
             y += 1
+        for x in range(1, max + 1):
+            if x not in tmp:
+                spacex.append(x)
+
+
+def expand(galist, spacex, spacey, value=1):
+    if value > 1:
+        value -=1
+    for x in reversed(spacex):
+        for g in galist:
+            if g[0] > x:
+                g[0] += value
+
+    for y in reversed(spacey):
+        for g in galist:
+            if g[1] > y:
+                g[1] += value
+
+    return galist
+
+
+def distance(g1, g2):
+    return abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
+
+
+def sumDistance(gallist):
+    sum = 0
+    for g1 in gallist:
+        for g2 in gallist:
+            sum += distance(g1, g2)
+    return sum // 2
 
 
 if __name__ == "__main__":
     loaddata("day11_input.txt")
-    print("part1: ")
+    galaxies = expand(galaxies, spacex, spacey, 1_000_000)
+    print("part1: ", sumDistance(galaxies))
--- test_day11.py
+++ test_day11.py
@@ -1,7 +1,35 @@
 from unittest import TestCase
 
+import day11
+
 
 class Test(TestCase):
 
     def test_loaddata(self):
-        self.fail()
+        day11.loaddata("day11_sample.txt")
+        expect = [[4, 1], [8, 2], [1, 3], [7, 5], [2, 6], [10, 7], [8, 9], [1, 10], [5, 10]]
+        self.assertEqual(expect, day11.galaxies)
+        self.assertEqual([3, 6, 9], day11.spacex)
+        self.assertEqual([4, 8], day11.spacey)
+
+    def test_expand(self):
+        gl = [[4, 1], [8, 2], [1, 3], [7, 5], [2, 6], [10, 7], [8, 9], [1, 10], [5, 10]]
+        spacex = [3, 6, 9]
+        spacey = [4, 8]
+        expect = [[5, 1], [10, 2], [1, 3], [9, 6], [2, 7], [13, 8], [10, 11], [1, 12], [6, 12]]
+        self.assertEqual(expect, day11.expand(gl, spacex, spacey))
+
+    def test_distance(self):
+        self.assertEqual(9, day11.distance([2, 7], [6, 12]))
+
+    def test_sum_distance(self):
+        day11.loaddata("day11_sample.txt")
+        galList = day11.expand(day11.galaxies, day11.spacex, day11.spacey)
+        self.assertEqual(374, day11.sumDistance(galList))
+
+    def test_sum_distance2(self):
+        day11.loaddata("day11_sample.txt")
+        # galList = day11.expand(day11.galaxies, day11.spacex, day11.spacey, 10)
+        # self.assertEqual(1030, day11.sumDistance(galList))
+        galList = day11.expand(day11.galaxies, day11.spacex, day11.spacey, 100)
+        self.assertEqual(8410, day11.sumDistance(galList))
