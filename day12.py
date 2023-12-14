dde954439be7f9a1eba39ecddfef198a34dcd4fa
Márton István <martoni779@gmail.com>
2023-12-14 21:20:28 +0100

    day12 part1 starts

--- day12.py
+++ day12.py
@@ -1,7 +1,12 @@
+
 def loaddata(filename):
     pass
 
 
+def arrangements(records):
+    pass
+
+
 if __name__ == "__main__":
-    loaddata('day12_input.txt')
-    print('Part1:')
+    records = loaddata('day12_input.txt')
+    print('Part1:', arrangements(records))
--- test_day12.py
+++ test_day12.py
@@ -1,6 +1,12 @@
 from unittest import TestCase
 
+import day12
+
 
 class Test(TestCase):
     def test_loaddata(self):
         self.fail()
+
+    def test_arrangements(self):
+        rec = []
+        self.assertEqual(21, day12.arrangements(rec))
