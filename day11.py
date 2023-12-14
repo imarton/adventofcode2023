e06130b1f553b203992b84ec4a39878b3b5583fb
imarton <imarton@users.noreply.github.com>
2023-12-14 16:53:35 +0100

    day11 part1 starts

--- day11.py
+++ day11.py
@@ -1,4 +1,12 @@
+def loaddata(filename):
+    with open(filename, 'r') as f:
+        y = 1
+        for row in f:
+            for x in range(len(row)):
+                pass
+            y += 1
 
 
 if __name__ == "__main__":
+    loaddata("day11_input.txt")
     print("part1: ")
