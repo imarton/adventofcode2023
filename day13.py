fd46fbaf2f4ca8c0aeefcc094c045e5ac2ff9c25
Márton István <martoni779@gmail.com>
2023-12-23 14:27:20 +0100

    day13 part0 starts

---
+++ day13.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+
+def findMirrors(param):
+    pass
+
+
+if __name__ == "__main__":
+    t0 = datetime.now()
+    print('start:', t0)
+    print('Part1:', findMirrors('day13_input.txt'))
+    t1 = datetime.now()
+    print('end1:', t1, ' elapsed time:', t1 - t0)
+    # print('Part2:', arrangements2('day12_input.txt'))
+    # t2 = datetime.now()
+    # print('end2:', t2, ' elapsed time:', t2 - t1)
