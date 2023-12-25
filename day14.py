e0fcb0aa6dade579fb3706947b56e760bc1ec3a0
Márton István <martoni779@gmail.com>
2023-12-25 14:07:45 +0100

    day14 part1 done

--- day14.py
+++ day14.py
@@ -1,16 +1,126 @@
 from datetime import datetime
 
 
-def totalload(param):
-    pass
+class Rock:
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        # self.top = None
+        # self.bottom = None
+        # self.left = None
+        # self.right = None
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.shape == other.shape
+
+    def __str__(self):
+        return f"Rock(x={self.x}, y={self.y} => {self.shape})"
+
+    __repr__ = __str__
+
+
+class Platform:
+    def __init__(self):
+        self.rocks = []
+        self.width = 0
+        self.height = 0
+
+    def getRockAt(self, x, y):
+        for r in self.rocks:
+            if r.x == x and r.y == y:
+                return r
+        return None
+
+    def getColAt(self, x):
+        col = []
+        for y in range(self.height + 1):
+            if r := self.getRockAt(x, y):
+                col.append(r)
+        return col
+
+    def getRowAt(self, y):
+        row = []
+        for x in range(self.width + 1):
+            if r := self.getRockAt(x, y):
+                row.append(r)
+        return row
+
+    def __eq__(self, other):
+        # return self.rocks == other.rocks and self.width == other.width and self.height == other.height
+        return self.__str__() == other.__str__()
+
+    def __str__(self):
+        table = [['.' for x in range(self.width)] for y in range(self.height)]
+        for r in self.rocks:
+            table[r.y - 1][r.x - 1] = r.shape
+
+        s = ''
+        for row in table:
+            s += ('\n' + ''.join(row))
+
+        return s
+
+    __repr__ = __str__
+
+
+def loadData(filename):
+    platform = Platform()
+    with open(filename, 'r') as f:
+        for y, row in enumerate(f, start=1):
+            for x, c in enumerate(row.strip(), start=1):
+                if c != '.':
+                    platform.rocks.append(Rock(x, y, c))
+        else:
+            platform.width = len(row)
+            platform.height = y
+
+    return platform
+
+
+def countload(platform):
+    sum = 0
+    for r in platform.rocks:
+        if r.shape == 'O':
+            sum += (platform.height - (r.y - 1))
+    return sum
+
+
+def tilt(platform, direction):
+    if direction in ('North', 'South'):
+        for x in range(1, platform.width + 1):
+            tilt_array(platform.getColAt(x), direction)
+    else:
+        pass
+    return platform
+
+
+def tilt_array(array, direction):
+    if direction == 'North':
+        array.sort(key=lambda r: r.y)
+
+        if array[0].shape == 'O' and array[0].y > 1:
+            array[0].y = 1
+
+        for i in range(1, len(array)):
+            if array[i].shape == '#':
+                continue
+            array[i].y = array[i - 1].y + 1
+
+    elif direction == 'South':
+        pass
+    elif direction == 'East':
+        pass
+    elif direction == 'West':
+        pass
+
+    return array
 
 
 if __name__ == "__main__":
-    t0 = datetime.now()
-    print('start:', t0)
-    print('Part1:', totalload('day14_input.txt'))  # 21272 is too low
-    t1 = datetime.now()
-    print('end1:', t1, ' elapsed time:', t1 - t0)
-    # print('Part2:', totalload('day14_input.txt'))
-    # t2 = datetime.now()
-    # print('end2:', t2, ' elapsed time:', t2 - t1)
+    p = loadData('day14_input.txt')
+    p = tilt(p, 'North')  # platform megdöntése északi irányba
+    # with open('day14_output.txt', 'w') as f:
+    #     print(p, file=f)
+
+    print('Part1:', countload(p))  # 102620 is too low
--- test_day14.py
+++ test_day14.py
@@ -1,8 +1,35 @@
 from unittest import TestCase
 
 import day14
+from day14 import Rock
 
 
 class Test(TestCase):
-    def test_totalload(self):
-        self.assertEqual(136, day14.totalload('day14_sample.txt'))
+    def test_countload(self):
+        p = day14.loadData('day14_sample.txt')
+        self.assertEqual(104, day14.countload(p))
+        p = day14.tilt(p, 'North')
+        self.assertEqual(136, day14.countload(p))
+
+    def test_tilt(self):
+        p = day14.loadData('day14_sample.txt')
+        pt = day14.loadData('day14_sample_tilted.txt')
+        result = day14.tilt(p, 'North')
+        self.assertEqual(pt, result)
+
+    def test_tilt_array(self):
+        t = [ Rock(1, 1, 'O')
+            , Rock(1, 2, 'O')
+            , Rock(1, 4, 'O')
+            , Rock(1, 6, 'O')
+            , Rock(1, 9, '#')
+            , Rock(1, 10, '#')]
+
+        expected = [ Rock(1, 1, 'O')
+            , Rock(1, 2, 'O')
+            , Rock(1, 3, 'O')
+            , Rock(1, 4, 'O')
+            , Rock(1, 9, '#')
+            , Rock(1, 10, '#')]
+
+        self.assertEqual(expected, day14.tilt_array(t, 'North'))
